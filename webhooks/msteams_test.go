31da46f7580b7c5cfbf4babca452e7d61a44641c
Timo Liebetrau <diiimo2k18@gmail.com>
2019-02-24 07:42:48 +0100

    update ms teams test

--- webhooks/msteams_test.go
+++ webhooks/msteams_test.go
@@ -5,31 +5,48 @@ import (
 	"github.com/litixsoft/lxgo/helper"
 	"github.com/litixsoft/lxgo/webhooks"
 	"github.com/stretchr/testify/assert"
-	"log"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestMsTeamsApi_SendSmall(t *testing.T) {
-	path := "/webhook/64112b37-462c-4c47"
+const (
+	Path = "/webhook/64112b37-462c-4c47"
+)
 
+var (
+	title = "Test99"
+	msg   = "test message"
+	color = lxWebhooks.RedDark
+)
+
+func TestMsTeamsApi_SendSmall(t *testing.T) {
 	// test server
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		// Test request parameters
-		assert.Equal(t, req.URL.String(), path)
+		assert.Equal(t, req.URL.String(), Path)
+		body, err := ioutil.ReadAll(req.Body)
+		assert.NoError(t, err)
 
-		// convert request body into map
-		cReqBody := new(lxHelper.M)
-		if err := json.NewDecoder(req.Body).Decode(cReqBody); err != nil {
-			log.Fatal(err)
+		// convert body for check
+		jsonBody := new(lxHelper.M)
+		assert.NoError(t, json.Unmarshal(body, jsonBody))
+
+		// expected map
+		expected := &lxHelper.M{
+			"@context":   "https://schema.org/extensions",
+			"@type":      "MessageCard",
+			"themeColor": color,
+			"title":      title,
+			"text":       msg,
 		}
 
-		// convert request body to json byte array
-		jbody, _ := json.Marshal(cReqBody)
+		// check request body
+		assert.Equal(t, expected, jsonBody)
 
-		// Send json request body back to client
-		rw.Write(jbody)
+		// Send http.StatusNoContent for successfully audit
+		rw.WriteHeader(http.StatusOK)
 	}))
 	// Close the server when test finishes
 	defer server.Close()
@@ -38,29 +55,10 @@ func TestMsTeamsApi_SendSmall(t *testing.T) {
 	api := &lxWebhooks.MsTeamsApi{
 		Client:  server.Client(),
 		BaseUrl: server.URL,
-		Path:    path,
+		Path:    Path,
 	}
-
-	// vars for test
-	title := "Test99"
-	msg := "test message"
-	color := lxWebhooks.RedDark
 
 	// send request with client
-	body, err := api.SendSmall(title, msg, color)
+	_, err := api.SendSmall(title, msg, color)
 	assert.NoError(t, err)
-
-	// check response
-	actual := new(lxHelper.M)
-	assert.NoError(t, json.Unmarshal(body, actual))
-
-	expected := &lxHelper.M{
-		"@context":   "https://schema.org/extensions",
-		"@type":      "MessageCard",
-		"themeColor": color,
-		"title":      title,
-		"text":       msg,
-	}
-
-	assert.Equal(t, expected, actual)
 }
