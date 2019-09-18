da7612e3ddc0c62a4a155686fbc62f9caf008eea
Diiimo2k18 <diiimo2k18@gmail.com>
2019-09-18 19:53:01 +0200

    update mongo unstable

--- db/mongo.go
+++ db/mongo.go
@@ -9,9 +9,20 @@ import (
 	"time"
 )
 
+const (
+	DefaultTimeout = time.Second * 30
+)
+
+type IMongoDBBaseRepo interface {
+	Find(collection string, filter interface{}, result interface{}, args ...interface{}) error
+}
+
+type mongoDbBaseRepo struct {
+	db *mongo.Database
+}
+
 // GetMongoDbClient, return new mongo driver client
 func GetMongoDbClient(uri string) (*mongo.Client, error) {
-
 	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
 	if err != nil {
 		return client, err
@@ -26,7 +37,40 @@ func GetMongoDbClient(uri string) (*mongo.Client, error) {
 	return client, nil
 }
 
+// NewMongoBaseRepo, return base repo instance
+func NewMongoBaseRepo(db *mongo.Database) IMongoDBBaseRepo {
+	return &mongoDbBaseRepo{db: db}
+}
+
+// Find, find all matched by filter
+func (repo *mongoDbBaseRepo) Find(collection string, filter interface{}, result interface{}, args ...interface{}) error {
+	// Default values
+	timeout := DefaultTimeout
+	opts := &options.FindOptions{}
+
+	for i := 0; i < len(args); i++ {
+		switch args[i].(type) {
+		case time.Duration:
+			timeout = args[0].(time.Duration)
+		case *options.FindOptions:
+			opts = args[0].(*options.FindOptions)
+		}
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	cur, err := repo.db.Collection(collection).Find(ctx, filter, opts)
+	if err != nil {
+		return err
+	}
+
+	return cur.All(ctx, result)
+}
+
+/////////////////////////////////////////////////
 // deprecated, Will be removed in a later version
+/////////////////////////////////////////////////
 // Db struct for mongodb
 type MongoDb struct {
 	Conn       *mgo.Session
--- db/mongo_test.go
+++ db/mongo_test.go
@@ -1,5 +1,22 @@
 package lxDb_test
 
+import (
+	"context"
+	"encoding/json"
+	lxDb "github.com/litixsoft/lxgo/db"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"io/ioutil"
+	"log"
+	"os"
+	"sort"
+	"testing"
+	"time"
+)
+
 //
 //import (
 //	"encoding/json"
@@ -16,7 +33,7 @@ package lxDb_test
 //	"time"
 //)
 //
-//// TestUser, struct for test users
+// TestUser, struct for test users
 //type TestUser struct {
 //	Id       bson.ObjectId `json:"id" bson:"_id"`
 //	Name     string        `json:"name" bson:"name"`
@@ -24,30 +41,38 @@ package lxDb_test
 //	Email    string        `json:"email" bson:"email"`
 //	IsActive bool          `json:"is_active" bson:"is_active"`
 //}
-//
-//const (
-//	TestDbName     = "lxgo_test"
-//	TestCollection = "users"
-//	FixturesPath   = "fixtures/users.test.json"
-//	T10 = time.Second * 10
-//)
-//
-//var (
-//	dbHost string
-//)
-//
-//func init() {
-//	// Check DbHost environment
-//	dbHost = os.Getenv("DB_HOST")
-//
-//	// When not defined set default host
-//	if dbHost == "" {
-//		dbHost = "mongodb://127.0.0.1:27017"
-//	}
-//
-//	log.Println("DB_HOST:", dbHost)
-//}
-//
+
+type TestUserNew struct {
+	Id       primitive.ObjectID `json:"id" bson:"_id"`
+	Name     string             `json:"name" bson:"name"`
+	Gender   string             `json:"gender" bson:"gender"`
+	Email    string             `json:"email" bson:"email"`
+	IsActive bool               `json:"is_active" bson:"is_active"`
+}
+
+const (
+	TestDbName     = "lxgo_test"
+	TestCollection = "users"
+	FixturesPath   = "fixtures/users.test.json"
+	T10            = time.Second * 10
+)
+
+var (
+	dbHost string
+)
+
+func init() {
+	// Check DbHost environment
+	dbHost = os.Getenv("DB_HOST")
+
+	// When not defined set default host
+	if dbHost == "" {
+		dbHost = "mongodb://127.0.0.1:27017"
+	}
+
+	log.Println("DB_HOST:", dbHost)
+}
+
 //// setupData, create the test data and prepare the database
 //func setupData(conn *mgo.Session) []TestUser {
 //	// Delete collection if exists
@@ -101,64 +126,49 @@ package lxDb_test
 //	return users
 //}
 //
-//// setupData, create the test data and prepare the database
-////func setupDataNew(db *mongo.Database) []TestUser {
-////	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-////	defer cancel()
-////
-////	// Delete collection if exists
-////	err := conn.DB(TestDbName).C(TestCollection).DropCollection()
-////	if err != nil {
-////		log.Fatal(err)
-////	}
-////
-////	collection := db.Collection(TestCollection)
-////	if err := collection.Drop()
-////
-////	// Setup indexes
-////	indexes := []mgo.Index{
-////		{Key: []string{"name"}},
-////		{Key: []string{"email"}, Unique: true},
-////	}
-////
-////	// Ensure indexes
-////	col := conn.DB(TestDbName).C(TestCollection)
-////	for _, i := range indexes {
-////		if err := col.EnsureIndex(i); err != nil {
-////			log.Fatal(err)
-////		}
-////	}
-////
-////	// Load test data from json file
-////	raw, err := ioutil.ReadFile(FixturesPath)
-////	if err != nil {
-////		log.Fatal(err)
-////	}
-////
-////	// Convert
-////	var users []TestUser
-////	if err := json.Unmarshal(raw, &users); err != nil {
-////		log.Fatal(err)
-////	}
-////
-////	// Make Test users map and insert test data in db
-////	for i := 0; i < len(users); i++ {
-////		users[i].Id = bson.NewObjectId()
-////
-////		// Insert user
-////		if err := conn.DB(TestDbName).C(TestCollection).Insert(users[i]); err != nil {
-////			log.Fatal(err)
-////		}
-////	}
-////
-////	// Sort test users with id for compare
-////	sort.Slice(users[:], func(i, j int) bool {
-////		return users[i].Id < users[j].Id
-////	})
-////
-////	// Return mongo connection
-////	return users
-////}
+// setupData, create the test data and prepare the database
+func setupDataNew(db *mongo.Database) []TestUserNew {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	// Delete collection if exists
+	collection := db.Collection(TestCollection)
+	if err := collection.Drop(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	// Load test data from json file
+	raw, err := ioutil.ReadFile(FixturesPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Convert
+	var users []TestUserNew
+	if err := json.Unmarshal(raw, &users); err != nil {
+		log.Fatal(err)
+	}
+
+	// Make Test users map and insert test data in db
+	for i := 0; i < len(users); i++ {
+		users[i].Id = primitive.NewObjectID()
+
+		// Insert user
+		_, err := collection.InsertOne(ctx, users[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Sort test users with id for compare
+	//sort.Slice(users[:], func(i, j int) bool {
+	//	return users[i].Id < users[j].Id
+	//})
+
+	// Return mongo connection
+	return users
+}
+
 //
 //func TestGetMongoDbClient(t *testing.T) {
 //	client, err := lxDb.GetMongoDbClient(dbHost)
@@ -235,16 +245,40 @@ package lxDb_test
 //}
 //
 /////// ############# ############# /////
-////func TestMongoBaseRepo(t *testing.T) {
-////	its := assert.New(t)
-////
-////	client, err := lxDb.GetMongoDbClient("mongodb://127.0.0.1")
-////	its.NoError(err)
-////	base := lxDb.NewMongoBaseRepo(client.Database(TestDbName))
-////
-////	filter := bson.D{}
-////	result := new(TestUser)
-////	err := base.Find(TestCollection)
-////
-////	t.Log(err)
-////}
+func TestMongoBaseRepo(t *testing.T) {
+	its := assert.New(t)
+
+	client, err := lxDb.GetMongoDbClient(dbHost)
+	its.NoError(err)
+
+	db := client.Database(TestDbName)
+	setupDataNew(db)
+	testUsers := setupDataNew(db)
+
+	// Sort result for compare
+	sort.Slice(testUsers[:], func(i, j int) bool {
+		return testUsers[i].Name < testUsers[j].Name
+	})
+
+	skip := 5
+	limit := 5
+	y := 0
+	var expectUsers []TestUserNew
+	for i, _ := range testUsers {
+		if i > (skip-1) && y < limit {
+			expectUsers = append(expectUsers, testUsers[i])
+			y++
+		}
+	}
+
+	base := lxDb.NewMongoBaseRepo(db)
+	filter := bson.D{}
+	var result []TestUserNew
+	findOptions := options.Find()
+	findOptions.SetSkip(5)
+	findOptions.SetLimit(5)
+	findOptions.SetSort(bson.M{"name": 1})
+	err = base.Find(TestCollection, filter, &result, findOptions)
+	its.NoError(err)
+	its.Equal(expectUsers, result)
+}
